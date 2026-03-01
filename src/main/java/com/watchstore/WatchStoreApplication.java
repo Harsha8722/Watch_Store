7ef1996011a6d0fd9f9abf551152cdf25126d1db
Harsha8722 <harshagowda8976@gmail.com>
2026-03-01 16:28:23 +0530

    Fix Dockerfile jar copy

--- src/main/java/com/watchstore/WatchStoreApplication.java
+++ src/main/java/com/watchstore/WatchStoreApplication.java
@@ -2,16 +2,9 @@ package com.watchstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WatchStoreApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(WatchStoreApplication.class);
-    }
+public class WatchStoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WatchStoreApplication.class, args);
